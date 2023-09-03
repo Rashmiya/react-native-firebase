38493da2ae8cfb1a9846ea9e15fba6509f176745
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-09-03 18:22:35 +0530

    profile updated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,46 @@
 /* eslint-disable react-native/no-inline-styles */
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, FormControl, Input, NativeBaseProvider} from 'native-base';
+import {firebase_auth, firestore_db} from '../../firebaseConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {collection, getDocs, query, where} from 'firebase/firestore';
 
-const Profile = () => {
-  function logOutOnAction(): void {
-    console.log('Done');
+const Profile = ({navigation}: any) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  async function logOutOnAction(): Promise<void> {
+    try {
+      await firebase_auth.signOut();
+      navigation.navigate('SignInPage');
+    } catch (e) {
+      console.log(e);
+    }
   }
+  useEffect(() => {
+    getUserDetails();
+  }, []);
 
+  async function getUserDetails() {
+    const userEmail = await AsyncStorage.getItem('userEmail');
+    console.log(userEmail);
+    const userCollectionRef = collection(firestore_db, 'SignInMembers');
+    const userQuery = query(userCollectionRef, where('email', '==', userEmail));
+    try {
+      const querySnapshot = await getDocs(userQuery);
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0];
+        const userData = userDoc.data();
+        setName(userData.name);
+        setEmail(userData.email);
+        console.log(userData);
+      } else {
+        console.log('User not found in Firestore.');
+      }
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  }
   return (
     <NativeBaseProvider>
       <View style={styles.layout}>
@@ -29,6 +62,7 @@ const Profile = () => {
                 isReadOnly={true}
                 style={styles.inputStyle}
                 size="md"
+                value={name}
                 placeholder=" Name"
                 borderColor={'#4b5563'}
                 borderRadius={10}
@@ -40,6 +74,7 @@ const Profile = () => {
                 isReadOnly={true}
                 style={styles.inputStyle}
                 size="md"
+                value={email}
                 placeholder=" example@abc.com"
                 borderColor={'#4b5563'}
                 borderRadius={10}
@@ -91,7 +126,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F9C86A',
     color: 'red',
     width: '100%',
-    top: 250,
+    top: 10,
   },
   logOutBtnTxt: {
     color: '#2E2D2D',
