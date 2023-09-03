18cdf8a73b5026d09115ed36755591d2cda09992
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-09-03 16:12:08 +0530

    firebase setup completed

---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-2LevMaotlUM5JMzVd1OXolMPFp3bAa0',
+  authDomain: 'authone-74980.firebaseapp.com',
+  projectId: 'authone-74980',
+  storageBucket: 'authone-74980.appspot.com',
+  messagingSenderId: '527977560048',
+  appId: '1:527977560048:web:518a85680d8a373a94cf35',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
